9aa1b6b6b302ad7a87883d49f96fb071cab60e78
rivaille-c <rivaille-c@mgdis.fr>
2016-08-16 14:07:32 +0200

    fix: redirect to main page

--- app/app.js
+++ app/app.js
@@ -1,7 +1,9 @@
 var app = angular.module('terriblelogos', ['ui.router', 'terriblelogos.directives', 'terriblelogos.services', 'terriblelogos.templates', 'bem']);
 
-app.config(['$stateProvider', function($stateProvider) {
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
   'use strict';
+
+  $urlRouterProvider.otherwise('/');
 
   $stateProvider.state('main', {
     url: '/?norainbow?text?interval',
